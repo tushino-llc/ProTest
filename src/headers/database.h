31a87af8cda4e563ef74dbfd741573705355bb26
Nonconformizt <bogdankorzh254@mail.ru>
2019-04-03 21:21:35 +0300

    Bogdan: Database module initialization, db_delete_question, db_delete_user (by Sanya), db_open

---
+++ src/headers/database.h
@@ -0,0 +1,14 @@
+
+/**
+*
+*   THIS FILE IS NOT LINKED TO MAIN HEADER YET
+*
+*/
+
+#include "sqlite.h"
+
+
+int db_open();
+
+int db_delete_question(int id);
+int db_delete_user(int id);
---
+++ src/libs/db/database.cpp
@@ -0,0 +1,46 @@
+
+sqlite3 * db = nullptr;
+
+
+int db_open()
+{
+    if(sqlite3_open("test.sqlite", &db) != SQLITE_OK)
+        return -1;
+    return 0;
+}
+
+int db_delete_question(int id)
+{
+    int rc;
+
+    sqlite3_stmt * st = nullptr;
+    rc = sqlite3_prepare_v2(db, "DELETE FROM `questions` WHERE id = ?", -1, &st, nullptr);
+    sqlite3_bind_int( st, 1, id );
+
+    if (rc != SQLITE_OK)
+        return -1;
+
+    rc= sqlite3_step(st);
+    if (rc != SQLITE_DONE && rc != SQLITE_OK)
+        return -1;
+
+    return 0;
+}
+
+int db_delete_user(int id)
+{
+    int rc;
+
+    sqlite3_stmt * st = nullptr;
+    rc = sqlite3_prepare_v2(db, "DELETE FROM `users` WHERE id = ?", -1, &st, nullptr);
+    sqlite3_bind_int( st, 1, id );
+
+    if (rc != SQLITE_OK)
+        return -1;
+
+    rc= sqlite3_step(st);
+    if (rc != SQLITE_DONE && rc != SQLITE_OK)
+        return -1;
+
+    return 0;
+}
