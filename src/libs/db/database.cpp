d9e377f231a1cad72fae0fdf91d58bf88a01bc40
Nonconformizt <bogdankorzh254@mail.ru>
2019-04-05 23:20:19 +0300

    Bogdan: db_get_users()
    db_get_users() function seems to be working, but has not been tested thoroughly yet

--- src/libs/db/database.cpp
+++ src/libs/db/database.cpp
@@ -309,11 +309,11 @@ int db_update_question(Question question)
     
     rc = sqlite3_prepare_v2(db, "UPDATE `questions` SET theme = ?, value = ?, ans1 = ?, ans2 = ?, ans3 = ?, ans4 = ?, correct = ? WHERE id = ?", -1, &st, nullptr);
     sqlite3_bind_int( st, 1, question.theme);
-    sqlite3_bind_text( st, 2, question.value, -1,SQLITE_STATIC);
-    sqlite3_bind_text( st, 3, question.ans[0], -1,SQLITE_STATIC);
-    sqlite3_bind_text( st, 4, question.ans[1], -1,SQLITE_STATIC);
-    sqlite3_bind_text( st, 5, question.ans[2], -1,SQLITE_STATIC);
-    sqlite3_bind_text( st, 6, question.ans[3], -1,SQLITE_STATIC);
+    sqlite3_bind_text( st, 2, question.value, -1, SQLITE_STATIC);
+    sqlite3_bind_text( st, 3, question.ans[0], -1, SQLITE_STATIC);
+    sqlite3_bind_text( st, 4, question.ans[1], -1, SQLITE_STATIC);
+    sqlite3_bind_text( st, 5, question.ans[2], -1, SQLITE_STATIC);
+    sqlite3_bind_text( st, 6, question.ans[3], -1, SQLITE_STATIC);
     sqlite3_bind_int( st, 7, question.correct );
     sqlite3_bind_int( st, 8, question.id );
 
@@ -329,4 +329,57 @@ int db_update_question(Question question)
         return -1;
 
     return SQLITE_OK;
+}
+
+User * db_get_users(int * size)
+{
+    sqlite3_stmt * st = nullptr;
+    *size = 0; // by default
+    
+    // get number of users
+    int rc = sqlite3_prepare_v2(db, "SELECT count(*) FROM `users`;", -1, &st, nullptr);
+
+    if (rc != SQLITE_OK)
+        return nullptr;
+    rc = sqlite3_step(st);
+    if (rc != SQLITE_DONE && rc != SQLITE_ROW)
+        return nullptr;
+
+    int n = sqlite3_column_int(st, 0);
+
+    if (n <= 0)
+        return nullptr;
+
+    *size = n;
+    auto users = new User[n];
+
+    rc = sqlite3_prepare_v2(db, "SELECT * FROM `users`;", -1, &st, nullptr);
+    if (rc != SQLITE_OK)
+        return nullptr;
+
+    int i = 0, num;
+    rc = sqlite3_step(st);
+
+    while (rc != SQLITE_DONE && rc != SQLITE_OK)
+    {
+        num = sqlite3_column_int(st, 0);
+        users[i].id = num;
+
+        auto text = (char *) sqlite3_column_text(st, 1);
+        strcpy(users[i].login, text);
+        // skip password column
+        text = (char *) sqlite3_column_text(st, 3);
+        strcpy( users[i].first_name, text );
+        text = (char *) sqlite3_column_text(st, 4);
+        strcpy( users[i].last_name, text );
+
+        num = sqlite3_column_int(st, 5);
+        users[i].admin = num != 0;
+
+        delete(text);
+        rc = sqlite3_step(st);
+        i++;
+    }
+
+    return users;
 }
