007681ab16a2bb5b54f982981ba8a99ba5c4323a
anton <anton2920@gmail.com>
2019-04-17 21:32:17 +0300

    Anton: fix some files

--- src/headers/tests_main_header.h
+++ src/headers/tests_main_header.h
@@ -1,14 +1,19 @@
-t — free powerful program for educational tests
+/*
+ProTest — free powerful program for educational tests
 Copyright © 2019 Tushino Software LLC
+
 This file is part of ProTest.
+
 ProTest is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
+
 ProTest is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
 along with ProTest. If not, see <https://www.gnu.org/licenses/>.
 */
@@ -25,26 +30,26 @@ along with ProTest. If not, see <https://www.gnu.org/licenses/>.
 /* Include general headers */
 #if (HAVE_CSTDIO == 1)
     #include <cstdio>
-	#endif
-	#if (HAVE_IOSTREAM == 1)
-	    #include <iostream>
-		#endif
-		#if (HAVE_CSTRING == 1)
-		    #include <cstring>
-			#endif
-			#if (HAVE_STDLIB_H == 1)
-			    #include <stdlib.h>
-				#endif
+#endif
+#if (HAVE_IOSTREAM == 1)
+    #include <iostream>
+#endif
+#if (HAVE_CSTRING == 1)
+    #include <cstring>
+#endif
+#if (HAVE_STDLIB_H == 1)
+    #include <stdlib.h>
+#endif
 
-				/* Include local headers */
-				#include "sqlite3.h"
-				#include "database.h"
-				#include "teacher_menu.h"
-				#include "../libs/libcrypt/include/tomcrypt.h"
-				#include "main_menu.h"
-				#include "student_menu.h"
+/* Include local headers */
+#include "sqlite3.h"
+#include "database.h"
+#include "teacher_menu.h"
+#include "../libs/libcrypt/include/tomcrypt.h"
+#include "main_menu.h"
+#include "student_menu.h"
 
-				/* qt_main.cpp */
-				int qt_main(int, char *[]);
+/* qt_main.cpp */
+int qt_main(int, char *[]);
 
-				#endif
+#endif
