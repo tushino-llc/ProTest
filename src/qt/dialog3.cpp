4e6502a5771d882d46466fab3554b7c7c86925f1
anton <anton2920@gmail.com>
2019-04-27 23:02:11 +0300

    Anton: UI licencing

--- src/qt/dialog3.cpp
+++ src/qt/dialog3.cpp
@@ -1,3 +1,23 @@
+/*
+ProTest — free powerful program for educational tests
+Copyright © 2019 Tushino Software LLC
+
+This file is part of ProTest.
+
+ProTest is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+ProTest is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ProTest. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "dialog3.h"
 #include "../build-qt-Desktop-Debug/ui_dialog3.h"
 #include "mainwindow_stud.h"
