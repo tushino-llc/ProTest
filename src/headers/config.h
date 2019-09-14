112f11e042cc9f83dc9f87f57027fd4c04379349
anton2920 <anton2920@gmail.com>
2019-09-14 22:12:50 +0300

    Update .gitignore

--- src/headers/config.h
+++ src/headers/config.h
@@ -7,9 +7,6 @@
 /* Define to 1 if you have the <cstring> header file. */
 #define HAVE_CSTRING 1
 
-/* Define to 1 if you have the <string> header file. */
-#define HAVE_STRING 1
-
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
@@ -22,11 +19,11 @@
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
-/* Define if AX_HAVE_QT macro found Qt */
+/* Define if Qt will be used */
 #define HAVE_QT 1
 
 /* Define to 1 if stdbool.h conforms to C99. */
-#define HAVE_STDBOOL_H 1
+/* #undef HAVE_STDBOOL_H */
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -34,8 +31,8 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
-/* Define to 1 if you have the <time.h> header file */
-#define HAVE_TIME_H 1
+/* Define to 1 if you have the <string> header file. */
+#define HAVE_STRING 1
 
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
@@ -48,6 +45,9 @@
 
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
